4bd665cfdd719eb197bbb847d6796fabfbb49683
Gwanghee Jo <92922343+Jojorabbit21@users.noreply.github.com>
2022-02-11 13:58:49 +0900

    Main

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return render_template('index.html')
+def gateway():
+  return render_template('index.html', title='index')
+
+@app.route('/main')
+def main():
+  return render_template('main.html', title='main')
 
 if __name__ == "__main__":
   app.run(host='127.0.0.1', port=5000, debug=True)
