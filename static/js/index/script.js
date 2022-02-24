14837eecdb8ff19df3add0d1db92b182d454bfa1
Gwanghee Jo <92922343+Jojorabbit21@users.noreply.github.com>
2022-02-24 17:54:32 +0900

    Main

--- static/js/index/script.js
+++ static/js/index/script.js
@@ -9,14 +9,14 @@ function slideMenu() {
   }
 }
 
-function zoomImage(element) {
+function zoomImage(pathname, element) {
   var filename = element.getAttribute('imgsrc');
   var modal = document.getElementsByClassName('modal')[0];
   var modal_image = document.getElementsByClassName('modal-image')[0];
   var status = modal.getAttribute('status');
 
   if(status == "hidden") {
-    modal_image.setAttribute('src',"/static/image/snap/" + filename);
+    modal_image.setAttribute('src',"/static/image" + pathname + '/' + filename);
     modal.setAttribute('status','visible');
   }
   else {
@@ -70,6 +70,16 @@ window.onload = function() {
     header.style.height = "auto";
     menu.style.height = vh + "px";
   }
+  else if(pathname == '/project') {
+    var header = document.querySelector('.header');
+    var menu = document.querySelector('.menu-nav');
+
+    document.body.style.width = vw + "px";
+    document.body.style.height = vh + "px";
+    header.style.width = vw + "px";
+    header.style.height = "auto";
+    menu.style.height = vh + "px";  
+  }
   else if(pathname.indexOf('/project/') > -1) {
     var header = document.querySelector('.header');
     var menu = document.querySelector('.menu-nav');
@@ -80,72 +90,11 @@ window.onload = function() {
     header.style.height = "auto";
     menu.style.height = vh + "px";
     
-    var project_container = document.querySelector('.project-img-container');
-    var project_contents = document.querySelectorAll('.project-img-box');
-    var totalWidth = 0;
-    for(var i=0; i<project_contents.length; i++) {
-      totalWidth += project_contents[i].offsetWidth;
-    }
-    project_container.style.width = totalWidth + 200 + "px";
+    // var project_contents = document.querySelectorAll('.swiper-slide');
+    // var project_img = document.querySelectorAll('.project-img');
+    // for(var i=0; i<project_contents.length; i++) {
+    //   project_contents[i].style.width = project_img[i].offsetWidth;
+    //   console.log(project_contents[i]);
+    // }
   }
 }
-
-let draggableSlider = function () {
-  // DOM element(s)
-  let slider = document.querySelector(".project-img-container"),
-    innerSlider = document.querySelector(".project-img-container-inner");
-
-  // Slider variables
-  let pressed = false,
-    startX,
-    x;
-
-  // Mousedown eventlistener
-  slider.addEventListener("mousedown", (e) => {
-    pressed = true;
-    startX = e.offsetX - innerSlider.offsetLeft;
-    slider.style.cursor = "grabbing";
-  });
-
-  // mouseneter
-  slider.addEventListener("mouseenter", () => {
-    slider.style.cursor = "grab";
-  });
-
-  // mouseup
-  slider.addEventListener("mouseup", () => {
-    slider.style.cursor = "grab";
-  });
-
-  // window
-  window.addEventListener("mouseup", () => {
-    pressed = false;
-  });
-
-  // Slider mousemove event listener
-  slider.addEventListener("mousemove", (e) => {
-    if (!pressed) return;
-    e.preventDefault();
-
-    x = e.offsetX;
-
-    innerSlider.style.left = `${x - startX}px`;
-
-    checkBoundry();
-  });
-
-  // Check boundry of outer and inner sliders
-  function checkBoundry() {
-    let outer = slider.getBoundingClientRect(),
-      inner = innerSlider.getBoundingClientRect();
-
-    if (parseInt(innerSlider.style.left) > 0) {
-      innerSlider.style.left = "0px";
-    } else if (inner.right < outer.right) {
-      innerSlider.style.left = `-${inner.width - outer.width}px`;
-    }
-  }
-};
-
-// Invoke code
-draggableSlider();
